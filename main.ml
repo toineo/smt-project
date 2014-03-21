fa20dd6b1c104d2b377d56c85da649fba95e9d25
kanunikov-denys <kanunikov.denis@gmail.com>
2014-03-21 17:07:19 +0100

    better ouf.test output

--- main.ml
+++ main.ml
@@ -10,19 +10,19 @@ let main () =
   in
   let lexbuf = Lexing.from_channel input_file
   in
-
   Solver.solve $ Parser.main Lexer.token lexbuf
     (* TODO: catch exceptions *)
-(* tests *)
 
+
+(* tests Union Find*)
 let t = create 10
-let () = assert (find t 0 <> find t 1)
+let () = Printf.printf "%b\n" (find t 0 <> find t 1)
 let t = union t 0 1
-let () = assert (find t 0 = find t 1)
-let () = assert (find t 0 <> find t 2)
-let t = union t 2 3 
+let () = Printf.printf "%b\n" (find t 0 = find t 1)
+let () = Printf.printf "%b\n" (find t 0 <> find t 2)
+let t = union t 2 3
 let t = union t 0 3
-let () = assert (find t 1 = find t 2)
+let () = Printf.printf "%b\n" (find t 1 = find t 2)
 let t = union t 4 4
-let () = assert (find t 4 <> find t 3)
-(* let () = assert (find t 4 = find t 3) *)
+let () = Printf.printf "%b\n" (find t 4 <> find t 3)
+let () = Printf.printf "%b\n" (find t 4 = find t 3)

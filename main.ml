7af04d2bb0c32713dd85c779c54ee968102260de
t <tux.toineo@gmail.com>
2014-03-24 04:06:23 -0400

    Modified main

--- main.ml
+++ main.ml
@@ -2,19 +2,10 @@
 open Operators
 open Puf
 
-let filename = Sys.argv.(1)
-
-
-let main () =
-  let input_file = open_in filename 
-  in
-  let lexbuf = Lexing.from_channel input_file
-  in
-  Solver.solve $ Parser.main Lexer.token lexbuf
-    (* TODO: catch exceptions *)
 
 
 (* tests Union Find*)
+
 let t = create 10
 let () = Printf.printf "%b\n" (find t 0 <> find t 1)
 let t = union t 0 1
@@ -26,3 +17,27 @@ let () = Printf.printf "%b\n" (find t 1 = find t 2)
 let t = union t 4 4
 let () = Printf.printf "%b\n" (find t 4 <> find t 3)
 let () = Printf.printf "%b\n" (find t 4 = find t 3)
+
+
+
+
+
+
+
+let () =
+  if Array.length Sys.argv < 2 then
+    begin
+      Printf.printf "Error: no input file given\n";
+      exit 1
+    end;
+
+  let filename = Sys.argv.(1)
+  in
+
+
+  let input_file = open_in filename
+  in
+  let lexbuf = Lexing.from_channel input_file
+  in
+  Solver.solve $ Parser.main Lexer.token lexbuf
+  (* TODO: catch exceptions *)
